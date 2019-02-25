187f036ed7d27c93329e680f1288f5f2aaf24da9
emirogs <emrehamurcu_1995@hotmail.com>
2019-02-25 23:11:37 +0300

    Initial commit

---
+++ app/src/main/java/com/emrhmrc/ocrwithtesseract/MainActivity.java
@@ -0,0 +1,161 @@
+package com.emrhmrc.ocrwithtesseract;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TESS_DATA = "/tessdata";
+    private static final String TAG = "MainActivity";
+    private static final String DATA_PATH = Environment.getExternalStorageDirectory().toString() + "/Tess";
+    private TextView txt_result;
+    private TessBaseAPI tessBaseAPI;
+    private Uri outputFileDir;
+    private String mCurrentPhotoPath;
+    private Button btn_open;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txt_result = findViewById(R.id.txt_result);
+        btn_open = findViewById(R.id.btn);
+        checkPermission();
+        btn_open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startCamera();
+            }
+        });
+    }
+
+    private void startCamera() {
+
+        try {
+            String imagePath = DATA_PATH + "/imgs";
+            File dir = new File(imagePath);
+            if (!dir.exists()) {
+                dir.mkdir();
+            }
+            String imageFilePath = imagePath + "/ocr.jpg";
+            outputFileDir = Uri.fromFile(new File(imageFilePath));
+            final Intent pictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileDir);
+            if (pictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(pictureIntent, 100);
+            }
+        } catch (Exception e) {
+            Log.d(TAG, "startCamera: " + e.getMessage());
+        }
+    }
+
+    private void checkPermission() {
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 120);
+        }
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 121);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+            if (resultCode == Activity.RESULT_OK) {
+                prepareTessData();
+                startOCR(outputFileDir);
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(getApplicationContext(), "Result canceled.", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Activity result failed.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void prepareTessData() {
+        try {
+            File dir = getExternalFilesDir(DATA_PATH + TESS_DATA);
+            if (!dir.exists()) {
+                if (!dir.mkdir()) {
+                    Toast.makeText(getApplicationContext(), "The folder " + dir.getPath() + "was not created", Toast.LENGTH_SHORT).show();
+                }
+            }
+            String fileList[] = getAssets().list("");
+            for (String fileName : fileList) {
+                String pathToDataFile = DATA_PATH + TESS_DATA + "/" + fileName;
+                if (!(new File(pathToDataFile)).exists()) {
+                    InputStream in = getAssets().open(fileName);
+                    OutputStream out = new FileOutputStream(pathToDataFile);
+                    byte[] buff = new byte[1024];
+                    int len;
+                    while ((len = in.read(buff)) > 0) {
+                        out.write(buff, 0, len);
+                    }
+                    in.close();
+                    out.close();
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
+    }
+
+    private void startOCR(Uri imageUri) {
+        try {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = false;
+            options.inSampleSize = 7;
+            Bitmap bitmap = BitmapFactory.decodeFile(imageUri.getPath(), options);
+            String result = this.getText(bitmap);
+            txt_result.setText(result);
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
+    }
+
+    private String getText(Bitmap bitmap) {
+        try {
+            tessBaseAPI = new TessBaseAPI();
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
+        tessBaseAPI.init(DATA_PATH, "tur");
+        tessBaseAPI.setImage(bitmap);
+        String retStr = "No result";
+        try {
+            retStr = tessBaseAPI.getUTF8Text();
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
+        tessBaseAPI.end();
+        return retStr;
+    }
+}
